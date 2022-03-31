1e285b45660846de7d6af9ae8e98ed93b648bd98
ComingDS <921940779@qq.com>
2022-03-31 16:31:52 +0800

    init

---
+++ static/js/37.530fb852.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbackstage_react_ts=self.webpackChunkbackstage_react_ts||[]).push([[37],{3037:function(e,t,s){s.r(t);s(2791);var a=s(1649),c=s(184);t.default=function(e){return(0,c.jsx)("div",{className:"Layout",children:(0,c.jsx)(a.Z,{routes:e.routes,redirect:"/layout/home"})})}}}]);
+//# sourceMappingURL=37.530fb852.chunk.js.map
